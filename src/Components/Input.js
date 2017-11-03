0b43b38b1a209d00676b66ed3fb8f179210708e1
Rafael Valdez <valdezr1@outlook.com>
2017-11-03 14:46:55 -0700

    Hard-coded data added. HTTP security issues with REST API. Left code commented to use as reference

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import { Button } from 'react-bootstrap'
+import { Button } from 'react-bootstrap';
+// import axios from 'axios';
+
+import exampleData from "./../Data.json"
 
 class Input extends Component {
 
@@ -16,23 +18,31 @@ class Input extends Component {
 
     grabInput(error){
         error.preventDefault();
-        axios({
-            method: 'get',
-            url: "http://quotes.stormconsultancy.co.uk/random.json",
-            headers : {
-                "Content-Type" : "application/json"
-            }
-        })
-            .then((response) => {
-                this.setState({
-                    quote : response.data.quote,
-                    author: "-" + response.data.author
-                });
-            })
-            .catch((error) => {
-                console.log(error)
-            });
+        let randIndex = Math.floor(Math.random() * exampleData.length)
+        this.setState({
+            quote : exampleData[randIndex].quote,
+            author: exampleData[randIndex].author
+        });
+        // this.setState({
+        //
+        // });
 
+        // axios({
+        //     method: 'get',
+        //     url: "http://quotes.stormconsultancy.co.uk/random.json",
+        //     headers : {
+        //         "Content-Type" : "application/json"
+        //     }
+        // })
+        // .then((response) => {
+        //     this.setState({
+        //         quote : response.data.quote,
+        //         author: "-" + response.data.author
+        //     });
+        // })
+        // .catch((error) => {
+        //     console.log(error)
+        // });
     }
 
     render() {
