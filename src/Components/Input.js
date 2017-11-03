1fa6985a820508c6210e9dd5500fbf20a033525a
Rafael Valdez <valdezr1@outlook.com>
2017-11-03 14:25:19 -0700

    header added in axios call

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -4,47 +4,50 @@ import { Button } from 'react-bootstrap'
 
 class Input extends Component {
 
-  constructor(){
-    super();
-    this.state = {
-      quote : "",
-      author: ""
+    constructor(){
+        super();
+        this.state = {
+            quote : "",
+            author: ""
+        }
+
+        this.grabInput = this.grabInput.bind(this);
     }
 
-    this.grabInput = this.grabInput.bind(this);
-  }
+    grabInput(error){
+        error.preventDefault();
+        axios({
+            method: 'get',
+            url: "http://quotes.stormconsultancy.co.uk/random.json",
+            headers : {
+                "Content-Type" : "application/json"
+            }
+        })
+            .then((response) => {
+                this.setState({
+                    quote : response.data.quote,
+                    author: "-" + response.data.author
+                });
+            })
+            .catch((error) => {
+                console.log(error)
+            });
 
-  grabInput(error){
-    error.preventDefault();
-    axios({
-      method: 'get',
-      url: "http://quotes.stormconsultancy.co.uk/random.json",
-    })
-    .then((response) => {
-      this.setState({
-        quote : response.data.quote,
-        author: "-" + response.data.author
-      });
-    })
-    .catch((error) => {
-      console.log(error)
-    });
+    }
 
-  }
-
-  render() {
-    return (
-      <div className= "quoteSpacing">
-        <form>
-          <Button bsStyle="primary" onClick={this.grabInput}>Generate Quote!</Button>
-        </form>
-        <hr/>
-          <h4><em>{this.state.quote}</em></h4>
-          <h5>{this.state.author}</h5>
-        <hr/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className= "quoteSpacing">
+                <form>
+                    <Button bsStyle="primary" onClick={this.grabInput}>Generate Quote!</Button>
+                </form>
+                <hr/>
+                <h4><em>{this.state.quote}</em></h4>
+                <h5>{this.state.author}</h5>
+                <hr/>
+            </div>
+        );
+    }
 }
 
 export default Input;
